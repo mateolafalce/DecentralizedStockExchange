74d47abc3c0d729339250ab65af6b8f0f8f01940
Mateo Lafalce <mateolafalce3@gmail.com>
2022-09-20 20:28:15 -0300

    20/9/2022

--- programs/decentralized_stock_exchange/src/lib.rs
+++ programs/decentralized_stock_exchange/src/lib.rs
@@ -1,1 +1,38 @@
-Actually private, sorry.
+use anchor_lang::{
+    prelude::*,
+    solana_program::account_info::AccountInfo,
+    solana_program::system_instruction,
+    solana_program::pubkey::Pubkey,
+}; 
+use std::str::FromStr;
+
+declare_id!("Ca8tecWTapYzeGfa8FvAMSo6JCheTRPvQhsjebZm56YE");
+
+#[program]
+pub mod solotery {
+    use super::*;
+    pub fn register_enterprise(
+        ctx: Context<RegisterEnterprise>
+    ) -> Result<()> {
+        let enterprise: &mut Account<Owners> = &mut ctx.accounts.solotery;
+        solotery.players = [].to_vec();
+        Ok(())
+    }
+}
+#[derive(Accounts)]
+pub struct RegisterEnterprise<'info> {
+    #[account(init, seeds = [user.key().as_ref()], bump, payer = user, space = 9612)]
+    pub enterprise: Account<'info, Owners>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct Owners {
+    pub users: Vec<Pubkey>, // 300 owners
+}
+#[error_code]
+pub enum ErrorCode {
+
+}
