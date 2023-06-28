3c40949f4e980680b75d6e467ebc5cb491dceda3
Mateo Lafalce <mateolafalce3@gmail.com>
2023-06-29 00:35:08 +1100

    initialize.rs

--- programs/decentralized_stock_exchange/src/instructions/initialize.rs
+++ programs/decentralized_stock_exchange/src/instructions/initialize.rs
@@ -1,27 +1,31 @@
 use anchor_lang::{
     prelude::*,
     solana_program::pubkey::Pubkey,
-}; 
+};
 use crate::state::accounts::*;
 
 pub fn initialize_decentralized_exchange_system(
     ctx: Context<Initialize>
 ) -> Result<()> {
+    // Get a mutable reference to the decentralized exchange system account
     let system: &mut Account<SystemExchangeAccount> = &mut ctx.accounts.decentralized_exchange_system;
+    // Find the program address using the seed "System Account" and program ID
     let (_pda, bump) = Pubkey::find_program_address(&[b"System Account"], ctx.program_id);
+    // Set the bump value for the system account
     system.bump_original = bump;
+    // Initialize various system account fields
     system.total_stock_companies = 0;
     system.historical_exchanges = 0;
     system.total_holders = 0;
     system.total_offers = 0;
-    Ok(())
+    Ok(()) 
 }
 
 #[derive(Accounts)]
 pub struct Initialize<'info> {
     #[account(init, seeds = [b"System Account"], bump, payer = user, space = SystemExchangeAccount::SIZE + 8)]
-    pub decentralized_exchange_system: Account<'info, SystemExchangeAccount>,
+    pub decentralized_exchange_system: Account<'info, SystemExchangeAccount>, // System account to be initialized
     #[account(mut)]
-    pub user: Signer<'info>,
-    pub system_program: Program<'info, System>,
+    pub user: Signer<'info>, // User account (mutable)
+    pub system_program: Program<'info, System>, // System program account
 }
