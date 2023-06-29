eec4a4bf29a5efeb1c927932257ce11cffa285d1
Mateo Lafalce <mateolafalce3@gmail.com>
2023-06-30 00:31:36 +1100

    init_buy_account.rs

--- programs/decentralized_stock_exchange/src/instructions/init_buy_account.rs
+++ programs/decentralized_stock_exchange/src/instructions/init_buy_account.rs
@@ -2,23 +2,32 @@ use anchor_lang::{
     prelude::*,
     solana_program::account_info::AccountInfo,
     solana_program::pubkey::Pubkey,
-}; 
+};
 use crate::state::accounts::*;
 use crate::errors::ErrorCode;
 
 pub fn init_buy_account(
-        ctx: Context<InitBuyAccount>
-    ) -> Result<()> {
-        let (_buy_pda, bump) = Pubkey::find_program_address(&[b"Buy Account", ctx.accounts.stock_account_pda.key().as_ref(), ctx.accounts.from.key().as_ref()], ctx.program_id);
-        require!(ctx.accounts.stock_account_pda.key() == ctx.accounts.stock_account.key(), ErrorCode::PubkeyError);
-        let buy_offer: &mut Account<SellOrBuyAccount> = &mut ctx.accounts.buy_offer; 
-        buy_offer.bump_original = bump;
-        buy_offer.sell_or_buy_amount = [].to_vec();
-        buy_offer.price = [].to_vec();
-        buy_offer.pubkey = ctx.accounts.from.key();
-        buy_offer.len = 65;
-        Ok(())
-    }
+    ctx: Context<InitBuyAccount>
+) -> Result<()> {
+    let (_buy_pda, bump) = Pubkey::find_program_address(
+        &[b"Buy Account", ctx.accounts.stock_account_pda.key().as_ref(), ctx.accounts.from.key().as_ref()],
+        ctx.program_id
+    );
+    // Ensure that the stock account PDA key matches the provided stock account key
+    require!(
+        ctx.accounts.stock_account_pda.key() == ctx.accounts.stock_account.key(),
+        ErrorCode::PubkeyError
+    );
+    let buy_offer: &mut Account<SellOrBuyAccount> = &mut ctx.accounts.buy_offer;
+    buy_offer.bump_original = bump;
+    // Initialize sell_or_buy_amount and price as empty vectors
+    buy_offer.sell_or_buy_amount = [].to_vec();
+    buy_offer.price = [].to_vec();
+    // Set the pubkey field of the buy offer account to the provided 'from' key
+    buy_offer.pubkey = ctx.accounts.from.key();
+    buy_offer.len = 65;
+    Ok(())
+}
 
 #[derive(Accounts)]
 pub struct InitBuyAccount<'info> {
