c966b9c5a8292a3832b1c9020e9edeb084a45922
Mateo Lafalce <mateolafalce3@gmail.com>
2023-07-01 00:18:43 +1100

    init_sell_account.rs

--- programs/decentralized_stock_exchange/src/instructions/init_sell_account.rs
+++ programs/decentralized_stock_exchange/src/instructions/init_sell_account.rs
@@ -2,24 +2,33 @@ use anchor_lang::{
     prelude::*,
     solana_program::account_info::AccountInfo,
     solana_program::pubkey::Pubkey,
-}; 
+};
 use crate::state::accounts::*;
 use crate::errors::ErrorCode;
 
 pub fn init_sell_account(
-        ctx: Context<InitSellAccount>
-    ) -> Result<()> {
-        let (_sell_pda, bump) = Pubkey::find_program_address(&[b"Sell Account", ctx.accounts.stock_account.key().as_ref(), ctx.accounts.from.key().as_ref()], ctx.program_id);
-        require!(ctx.accounts.stock_account_pda.key() == ctx.accounts.stock_account.key(), ErrorCode::PubkeyError);
-        require!(ctx.accounts.stock_account_pda.key() == ctx.accounts.stock_account.key(), ErrorCode::PubkeyError);
-        let sell_offer: &mut Account<SellOrBuyAccount> = &mut ctx.accounts.sell_offer; 
-        sell_offer.bump_original = bump;
-        sell_offer.sell_or_buy_amount = [].to_vec();
-        sell_offer.price = [].to_vec();
-        sell_offer.pubkey = ctx.accounts.from.key();
-        sell_offer.len = 65;
-        Ok(())
-    }
+    ctx: Context<InitSellAccount>
+) -> Result<()> {
+    let (_sell_pda, bump) = Pubkey::find_program_address(&[
+        b"Sell Account",
+        ctx.accounts.stock_account.key().as_ref(),
+        ctx.accounts.from.key().as_ref()
+    ], ctx.program_id);
+    // Ensure that the stock account PDA is the same as the stock account
+    require!(ctx.accounts.stock_account_pda.key() == ctx.accounts.stock_account.key(), ErrorCode::PubkeyError);
+    // Ensure that the stock account PDA is the same as the stock account
+    require!(ctx.accounts.stock_account_pda.key() == ctx.accounts.stock_account.key(), ErrorCode::PubkeyError);
+    let sell_offer: &mut Account<SellOrBuyAccount> = &mut ctx.accounts.sell_offer;
+    // Set the original bump value for the sell offer account
+    sell_offer.bump_original = bump;
+    // Initialize sell_or_buy_amount and price fields as empty vectors
+    sell_offer.sell_or_buy_amount = [].to_vec();
+    sell_offer.price = [].to_vec();
+    // Set the pubkey field of the sell offer account as the "from" account's key
+    sell_offer.pubkey = ctx.accounts.from.key();
+    sell_offer.len = 65;
+    Ok(())
+}
 
 #[derive(Accounts)]
 pub struct InitSellAccount<'info> {
